74198d88e875003d5db2dc427ea7dd9a0b6e84cc
Lucas Moreira <moreirapontocom@gmail.com>
2025-06-03 21:45:23 -0700

    Tooltip placement

--- src/components/TextWithTooltip/TextWithTooltip.component.tsx
+++ src/components/TextWithTooltip/TextWithTooltip.component.tsx
@@ -1,11 +1,11 @@
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 function TextWithTooltip(props: any): any {
-    const { id, tooltip, children } = props;
+    const { id, tooltip, placement, children } = props;
     return (
       <OverlayTrigger
           overlay={<Tooltip id={id}>{tooltip}</Tooltip>}
-          placement="top">
+          placement={placement || "top"}>
             <span>{children}</span>
       </OverlayTrigger>
     );
