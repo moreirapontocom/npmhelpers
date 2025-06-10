996442abe6914285d2a9d409ec173370c251f8c7
Lucas Moreira <moreirapontocom@gmail.com>
2025-06-10 16:59:25 -0300

    Fix phoneNumber

--- src/helpers/formatPhoneNumber.ts
+++ src/helpers/formatPhoneNumber.ts
@@ -1,12 +1,12 @@
 const formatPhoneNumber = (phone: string) => {
   if (!phone) return phone;
   const cleaned = phone.replace(/\D/g, "");
-  // Suporta: +99 (99) 9999-9999 ou +99 (99) 99999-9999
-  const match = cleaned.match(/^(\d{1,3})(\d{2})(\d{4,5})(\d{4})$/);
+  // Suporta: 99 (99) 9999-9999 ou 99 (99) 99999-9999
+  const match = cleaned.match(/^(\d{1,2})(\d{2})(\d{4,5})(\d{4})$/);
   if (match) {
     return `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}`;
   }
   return cleaned;
 };
 
-export default formatPhoneNumber;
+export default formatPhoneNumber; 
